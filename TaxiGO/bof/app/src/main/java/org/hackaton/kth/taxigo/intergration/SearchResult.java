024fc71ad31c8010a54613d785361a488d7b3946
fhiden <fhiden@kth.se>
2017-04-27 11:43:26 +0100

    added the class SearchResult for summorizing collected data

---
+++ TaxiGO/bof/app/src/main/java/org/hackaton/kth/taxigo/intergration/SearchResult.java
@@ -0,0 +1,26 @@
+package org.hackaton.kth.taxigo.intergration;
+
+/**
+ * Created by fhiden on 2017-04-27.
+ */
+
+public class SearchResult {
+    private Double cost;
+    private String company;
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
